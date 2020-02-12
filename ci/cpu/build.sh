c6b3d3b065f2adf4d028b780a26d3b4253e75ab2
Mike Wendt <mike.wendt@me.com>
2020-02-11 21:19:38 -0500

    ENH Change script to build all meta pkgs in one job

--- ci/cpu/build.sh
+++ ci/cpu/build.sh
@@ -36,11 +36,11 @@ conda list --show-channel-urls
 function build_pkg {
   # Build pkg
   gpuci_logger "Start conda build for '$CONDA_RECIPE'..."
-  conda build -c ${CONDA_USERNAME:-rapidsai-nightly} -c nvidia -c conda-forge --python=${PYTHON_VERSION} ${CONDA_RECIPE)
+  conda build -c ${CONDA_USERNAME:-rapidsai-nightly} -c nvidia -c conda-forge --python=${PYTHON_VERSION} ${1)
 
   # Get output location
   gpuci_logger "Get conda build output..."
-  export UPLOADFILE=`conda build -c ${CONDA_USERNAME:-rapidsai-nightly} -c nvidia -c conda-forge --python=${PYTHON_VERSION} ${CONDA_RECIPE) --output`
+  export UPLOADFILE=`conda build -c ${CONDA_USERNAME:-rapidsai-nightly} -c nvidia -c conda-forge --python=${PYTHON_VERSION} ${1) --output`
   test -e ${UPLOADFILE}
 
   # Check for upload key
@@ -59,14 +59,21 @@ function build_pkg {
   conda build purge
 }
 
-# Start build
-build_pkg
+function build_default_pkg {
+  # Build default version if current version matches DEFAULT_CUDA_VERSION
+  if [ "$CUDA_VESION" == "$DEFAULT_CUDA_VERSION" ] ; then
+    gpuci_logger "Current CUDA_VERSION '$CUDA_VERSION' is the DEFAULT_CUDA_VERSION, building package again with incremented build number..."
+    gpuci_logger "Previous build number '$RAPIDS_OFFSET'"
+    export RAPIDS_OFFSET=$((RAPIDS_OFFSET+1))
+    gpuci_logger "New build number '$RAPIDS_OFFSET'"
+    build_pkg $1
+  else
+    gpuci_logger "Current CUDA_VERSION '$CUDA_VERSION' is not DEFAULT_CUDA_VERSION, skipping default build..."
+  fi
+}
 
-# Build default version if current version matches DEFAULT_CUDA_VERSION
-if [ "$CUDA_VESION" == "$DEFAULT_CUDA_VERSION" ] ; then
-  gpuci_logger "Current CUDA_VERSION '$CUDA_VERSION' is the DEFAULT_CUDA_VERSION, building package again with incremented build number..."
-  gpuci_logger "Previous build number '$RAPIDS_OFFSET'"
-  export RAPIDS_OFFSET=$((RAPIDS_OFFSET+1))
-  gpuci_logger "New build number '$RAPIDS_OFFSET'"
-  build_pkg
-fi
+# Run builds
+build_pkg $CONDA_XGBOOST_RECIPE
+build_default_pkg $CONDA_XGBOOST_RECIPE
+build_pkg $CONDA_RAPIDS_RECIPE
+build_default_pkg $CONDA_RAPIDS_RECIPE
