296ce71e11f8ae02cdf1d4b4c63f862b3362d529
dillon-cullinan <dcullinan92@gmail.com>
2020-07-24 11:23:07 -0700

    Cleanup test scripts

--- ci/test/cugraph.sh
+++ ci/test/cugraph.sh
@@ -13,8 +13,6 @@ bash ./get_test_data.sh
 export RAPIDS_DATASET_ROOT_DIR=/rapids/cugraph/datasets
 
 # Install test deps
-conda install -y -c conda-forge -c defaults python-louvain networkx
-# FIXME: Install the master version of dask, distributed, and streamz
 pip install "git+https://github.com/dask/distributed.git" --upgrade --no-deps
 pip install "git+https://github.com/dask/dask.git" --upgrade --no-deps
 env
@@ -27,13 +25,8 @@ SUITEERROR=0
 # gtests
 for gt in /rapids/cugraph/cpp/build/gtests/*_TEST; do
    # FIXME: remove this ASAP
-   if [[ ${gt} == "/rapids/cugraph/cpp/build/gtests/SNMG_SPMV_TEST" ]]; then
-      ${gt} --gtest_output=xml:${TESTRESULTS_DIR}/ --gtest_filter=-hibench_test/Tests_MGSpmv_hibench.CheckFP32_hibench*
-      exitcode=$?
-   else
-      ${gt} --gtest_output=xml:${TESTRESULTS_DIR}/
-      exitcode=$?
-   fi
+   ${gt} --gtest_output=xml:${TESTRESULTS_DIR}/
+   exitcode=$?
    if (( ${exitcode} != 0 )); then
       SUITEERROR=${exitcode}
       echo "FAILED: ${gt}"
