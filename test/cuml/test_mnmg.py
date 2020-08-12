959e5667e8b8cceeb61bfbe46d0bea2897eb2820
Nanthini Balasubramanian <nathanb@nvidia.com>
2020-08-12 22:56:04 +0000

    UPDATE n_workers

--- test/cuml/test_mnmg.py
+++ test/cuml/test_mnmg.py
@@ -7,7 +7,8 @@ import dask_cudf
 
 def test_mnmg():
     cluster = LocalCUDACluster(threads_per_worker=1)
-    _ = Client(cluster)
+    client = Client(cluster)
+    n_workers = len(client.scheduler_info()['workers'])
 
     # Create and populate a GPU DataFrame
     df_float = cudf.DataFrame()
@@ -15,16 +16,13 @@ def test_mnmg():
     df_float['1'] = [4.0, 2.0, 1.0]
     df_float['2'] = [4., 2, 1]
 
-    ddf_float = dask_cudf.from_cudf(df_float, npartitions=2)
+    ddf_float = dask_cudf.from_cudf(df_float, npartitions=2*n_workers)
 
     X = ddf_float[ddf_float.columns.difference(['2'])]
     y = ddf_float['2']
     mod = LinearRegression()
     mod = mod.fit(X, y)
 
-    actual_output = str(mod.predict(X).compute())
-    expected_output = """0    4.0
-1    2.0
-2    1.0
-dtype: float64"""
+    actual_output = str(mod.predict(X).compute().values)
+    expected_output = '[4. 2. 1.]'
     assert actual_output == expected_output
