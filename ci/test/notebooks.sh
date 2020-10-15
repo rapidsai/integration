3f6130527971704751c7e969f983af8c41b88c03
AJ Schmidt <aschmidt@nvidia.com>
2020-10-15 11:40:02 -0400

    add pytorch to ci/test/notebooks.sh

--- ci/test/notebooks.sh
+++ ci/test/notebooks.sh
@@ -6,6 +6,11 @@ set -o pipefail
 export LIBCUDF_KERNEL_CACHE_PATH=${WORKSPACE}/.jitcache
 
 source /opt/conda/bin/activate rapids
+
+# PyTorch is intentionally excluded from our Docker images due
+# to its size, but some notebooks still depend on it.
+conda install -y -c pytorch pytorch
+
 env
 /test.sh 2>&1 | tee nbtest.log
 EXITCODE=$?
