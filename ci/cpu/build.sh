3be923c6acc1a3b27551dca9113ff03eae4cc20f
Jordan Jacobelli <jjacobelli@nvidia.com>
2021-04-28 15:26:26 +0200

    ENH Remove defaults channel from conda build
    Signed-off-by: Jordan Jacobelli <jjacobelli@nvidia.com>

--- ci/cpu/build.sh
+++ ci/cpu/build.sh
@@ -53,13 +53,13 @@ function build_pkg {
   # Build pkg
   gpuci_logger "Start conda build for '${1}'..."
   if [[ "${1}" == "${CONDA_BLAZING_NOTEBOOK_RECIPE}" ]]; then
-    gpuci_conda_retry build --override-channels -c blazingsql-nightly -c ${CONDA_USERNAME:-rapidsai-nightly} -c nvidia -c pytorch -c conda-forge -c defaults \
+    gpuci_conda_retry build --override-channels -c blazingsql-nightly -c ${CONDA_USERNAME:-rapidsai-nightly} -c nvidia -c pytorch -c conda-forge \
                 --python=${PYTHON_VER} -m ${CONDA_CONFIG_FILE} ${1}
   elif [[ "${1}" == *"BLAZING"* ]]; then
-    gpuci_conda_retry build --override-channels -c blazingsql-nightly -c ${CONDA_USERNAME:-rapidsai-nightly} -c nvidia -c conda-forge -c defaults \
+    gpuci_conda_retry build --override-channels -c blazingsql-nightly -c ${CONDA_USERNAME:-rapidsai-nightly} -c nvidia -c conda-forge \
                 --python=${PYTHON_VER} -m ${CONDA_CONFIG_FILE} ${1}
   else
-    gpuci_conda_retry build --override-channels -c ${CONDA_USERNAME:-rapidsai-nightly} -c nvidia -c conda-forge -c defaults \
+    gpuci_conda_retry build --override-channels -c ${CONDA_USERNAME:-rapidsai-nightly} -c nvidia -c conda-forge \
                 --python=${PYTHON_VER} -m ${CONDA_CONFIG_FILE} ${1}
   fi
 }
