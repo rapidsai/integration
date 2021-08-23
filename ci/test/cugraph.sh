3a0cad9a0d087fc6a25b49a1c0ad605bb4af992e
Joseph <68436579+jolorunyomi@users.noreply.github.com>
2021-08-23 15:48:39 -0500

    Update cugraph.sh

--- ci/test/cugraph.sh
+++ ci/test/cugraph.sh
@@ -2,5 +2,10 @@
 set +e
 set -x
 
+export HOME="${WORKSPACE}"
+export LIBCUDF_KERNEL_CACHE_PATH="${WORKSPACE}/.jitcache"
+export PATH="/opt/conda/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/local/gcc7/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+
 cd /rapids/cugraph
 bash /rapids/cugraph/ci/test.sh
