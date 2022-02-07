07af5856e275d99991089a3a6299d7bd6165d5f6
Jordan Jacobelli <jjacobelli@nvidia.com>
2022-02-07 16:47:12 +0100

    Use mambabuild
    Signed-off-by: Jordan Jacobelli <jjacobelli@nvidia.com>

--- ci/cpu/build.sh
+++ ci/cpu/build.sh
@@ -33,7 +33,9 @@ source activate base
 env
 
 # Install gpuCI tools
-conda install -y -c gpuci gpuci-tools
+#conda install -y -c gpuci gpuci-tools
+conda create -n tmp -c gpuci -c conda-forge gpuci-tools boa
+conda activate tmp
 
 # Print diagnostic information
 gpuci_logger "Print conda info..."
@@ -52,7 +54,7 @@ ARCH=$(uname -m)
 function build_pkg {
   # Build pkg
   gpuci_logger "Start conda build for '${1}'..."
-  gpuci_conda_retry build --override-channels -c ${CONDA_USERNAME:-rapidsai-nightly} -c nvidia -c conda-forge \
+  gpuci_conda_retry mambabuild --override-channels -c ${CONDA_USERNAME:-rapidsai-nightly} -c nvidia -c conda-forge \
     --python=${PYTHON_VER} -m ${CONDA_CONFIG_FILE} ${1}
 }
 
