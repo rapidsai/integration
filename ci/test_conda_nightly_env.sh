c245fc6cb1623d425e39d3bc9aaff04e7f5cbf36
Bradley Dice <bdice@bradleydice.com>
2024-08-29 13:37:37 -0500

    Clean up.

--- ci/test_conda_nightly_env.sh
+++ ci/test_conda_nightly_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2023, NVIDIA CORPORATION.
+# Copyright (c) 2024, NVIDIA CORPORATION.
 
 set -euo pipefail
 
@@ -10,11 +10,8 @@ JSON_FILENAME="rapids_cuda${CUDA_VERSION}_py${RAPIDS_PY_VERSION}.json"
 
 rapids-logger "Creating conda environment with rapids=${RAPIDS_VERSION}, python=${RAPIDS_PY_VERSION}, cuda-version=${CUDA_VERSION}"
 
-echo "::group::conda dry-run output"
-
 rapids-conda-retry \
     create \
-    --solver=libmamba \
     -n rapids-${RAPIDS_VERSION} \
     -c rapidsai-nightly \
     -c conda-forge \
@@ -25,8 +22,6 @@ rapids-conda-retry \
     --dry-run \
     --json \
     | tee "${JSON_FILENAME}"
-
-echo "::endgroup::"
 
 rapids-logger "Parsing results from conda dry-run with rapids=${RAPIDS_VERSION}, python=${RAPIDS_PY_VERSION}, cuda-version=${CUDA_VERSION}"
 
