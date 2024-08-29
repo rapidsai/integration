4aeb2f5e9504d7a3b93ef412c58d68479df06b74
Bradley Dice <bdice@bradleydice.com>
2024-08-29 15:44:57 -0500

    Update colors.

--- ci/check_conda_nightly_env.py
+++ ci/check_conda_nightly_env.py
@@ -25,9 +25,9 @@ EXCLUDED_PACKAGES = {
 }
 
 # ANSI color codes used to highlight lines
-FAIL = "\033[91m"
-WARNING = "\033[93m"
-OKGREEN = "\033[92m"
+FAIL = "\033[31m"
+WARNING = "\033[33m"
+OKGREEN = "\033[32m"
 ENDC = "\033[0m"
 
 
