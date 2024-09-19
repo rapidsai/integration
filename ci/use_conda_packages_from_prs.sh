de8c29c9f59caf4a0d6fc37b77877a1ef030893f
James Lamb <jlamb@nvidia.com>
2024-09-19 10:25:50 -0500

    try adding back some alpha floors

--- ci/use_conda_packages_from_prs.sh
+++ ci/use_conda_packages_from_prs.sh
@@ -8,19 +8,23 @@ CUDF_PYTHON_CHANNEL=$(rapids-get-pr-conda-artifact cudf 16806 python)
 
 UCXX_CHANNEL=$(rapids-get-pr-conda-artifact ucxx 278 cpp)
 
-# LIBRAFT_CHANNEL=$(rapids-get-pr-conda-artifact raft 2433 cpp)
-# RAFT_CHANNEL=$(rapids-get-pr-conda-artifact raft 2433 python)
-
 LIBCUSPATIAL_CHANNEL=$(rapids-get-pr-conda-artifact cuspatial 1441 cpp)
 CUSPATIAL_CHANNEL=$(rapids-get-pr-conda-artifact cuspatial 1441 python)
+
+LIBRAFT_CHANNEL=$(rapids-get-pr-conda-artifact raft 2433 cpp)
+RAFT_CHANNEL=$(rapids-get-pr-conda-artifact raft 2433 python)
+
+LIBCUGRAPH_CHANNEL=$(rapids-get-pr-conda-artifact cugraph 4655 cpp)
+CUGRAPH_CHANNEL=$(rapids-get-pr-conda-artifact cugraph 4655 python)
 
 conda config --system --add channels "${LIBRMM_CHANNEL}"
 conda config --system --add channels "${RMM_CHANNEL}"
 conda config --system --add channels "${CUDF_CPP_CHANNEL}"
 conda config --system --add channels "${CUDF_PYTHON_CHANNEL}"
 conda config --system --add channels "${UCXX_CHANNEL}"
-# conda config --system --add channels "${LIBRAFT_CHANNEL}"
-# conda config --system --add channels "${RAFT_CHANNEL}"
 conda config --system --add channels "${LIBCUSPATIAL_CHANNEL}"
 conda config --system --add channels "${CUSPATIAL_CHANNEL}"
-
+conda config --system --add channels "${LIBRAFT_CHANNEL}"
+conda config --system --add channels "${RAFT_CHANNEL}"
+conda config --system --add channels "${LIBCUGRAPH_CHANNEL}"
+conda config --system --add channels "${CUGRAPH_CHANNEL}"
