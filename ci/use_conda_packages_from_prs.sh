626bcb7532341c3269342beae4f1cfe7dbebcf2b
James Lamb <jlamb@nvidia.com>
2024-09-23 14:11:44 -0500

    add cuml and cumlprims_mg artifacts

--- ci/use_conda_packages_from_prs.sh
+++ ci/use_conda_packages_from_prs.sh
@@ -14,6 +14,15 @@ RAFT_CHANNEL=$(rapids-get-pr-conda-artifact raft 2433 python)
 LIBCUSPATIAL_CHANNEL=$(rapids-get-pr-conda-artifact cuspatial 1441 cpp)
 CUSPATIAL_CHANNEL=$(rapids-get-pr-conda-artifact cuspatial 1441 python)
 
+LIBCUML_CHANNEL=$(rapids-get-pr-conda-artifact cuspatial 6071 cpp)
+CUML_CHANNEL=$(rapids-get-pr-conda-artifact cuspatial 6071 python)
+
+# NOTE: cloning private repos with rapids-get-pr-conda-artifact doesn't work,
+#       so need to explicitly set the SHA to use
+CUMLPRIMS_CHANNEL=$(
+    RAPIDS_SHA=6f9f474 rapids-get-pr-conda-artifact cumlprims_mg 211 cpp 6f9f474
+)
+
 conda config --system --add channels "${LIBRMM_CHANNEL}"
 conda config --system --add channels "${RMM_CHANNEL}"
 conda config --system --add channels "${CUDF_CPP_CHANNEL}"
@@ -23,4 +32,6 @@ conda config --system --add channels "${LIBRAFT_CHANNEL}"
 conda config --system --add channels "${RAFT_CHANNEL}"
 conda config --system --add channels "${LIBCUSPATIAL_CHANNEL}"
 conda config --system --add channels "${CUSPATIAL_CHANNEL}"
-
+conda config --system --add channels "${LIBCUML_CHANNEL}"
+conda config --system --add channels "${CUML_CHANNEL}"
+conda config --system --add channels "${CUMLPRIMS_CHANNEL}"
