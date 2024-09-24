dee7735f56532e9d482428da16d5b37d909c28d2
James Lamb <jlamb@nvidia.com>
2024-09-24 08:21:39 -0500

    revert testing-only changes

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -11,8 +11,6 @@ source rapids-date-string
 
 CONDA_CONFIG_FILE="conda/recipes/versions.yaml"
 
-source ./ci/use_conda_packages_from_prs.sh
-
 rapids-print-env
 
 rapids-logger "Build rapids-xgboost"
