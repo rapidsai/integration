5b128ace143c16ebebfb327352c2f440dd2562d1
Gil Forsyth <gforsyth@nvidia.com>
2026-04-13 17:06:31 -0400

    feat(imports): finish import tests but raise if any fail along the way

--- ci/stable_install/test_imports.sh
+++ ci/stable_install/test_imports.sh
@@ -5,17 +5,27 @@ set -euo pipefail
 
 function testImports {
     local -a imports=()
+    local failures=0
     while [[ $# -gt 0 ]]; do
         # run standalone import test
         rapids-logger "Standalone import test for $1"
-        python -c "import $1" || rapids-logger "Test failed for: $1"
-        rapids-logger "Passed"
+        if python -c "import $1"; then
+            rapids-logger "Passed"
+        else
+            rapids-logger "Test failed for: $1"
+            failures=$((failures + 1))
+        fi
         # add import to array for combined import test before shifting
         imports+=("$1")
         shift
     done
     import_cmd=$(printf "import %s; " "${imports[@]}")
     rapids-logger "Combined import test for: ${imports[*]}"
-    python -c "${import_cmd}"
-    rapids-logger "Passed"
+    if python -c "${import_cmd}"; then
+        rapids-logger "Passed"
+    else
+        rapids-logger "Combined import test failed"
+        failures=$((failures + 1))
+    fi
+    return $failures
 }
