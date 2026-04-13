0e78bc01260a8f68375b510de57b093a00ffb672
Gil Forsyth <gforsyth@nvidia.com>
2026-04-13 15:20:33 -0400

    refactor: use `rapidsai/citestwheel` instead of `nvidia/cuda`

--- ci/stable_install/bootstrap/pip.sh
+++ ci/stable_install/bootstrap/pip.sh
@@ -3,12 +3,6 @@
 
 set -euo pipefail
 
-apt update && apt install -y curl
-
-echo "installing 'gha-tools'"
-curl -fsSL https://github.com/rapidsai/gha-tools/releases/latest/download/tools.tar.gz | tar -xz -C /usr/local/bin/
-echo "done installing 'gha-tools' to '/usr/local/bin'"
-
 echo "installing 'uv'"
 curl -LsSf https://astral.sh/uv/install.sh | sh
 echo "done installing 'uv'"
