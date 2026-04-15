d518002ea7f1a760a1b431aa1344f0feeffa10cf
Gil Forsyth <gforsyth@nvidia.com>
2026-04-15 09:56:16 -0400

    fix: remove nightly index from pip config

--- ci/stable_install/bootstrap/pip.sh
+++ ci/stable_install/bootstrap/pip.sh
@@ -8,3 +8,10 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 echo "done installing 'uv'"
 
 source "$HOME"/.local/bin/env
+
+# Nuke existing config (pip config --global doesn't touch this file for some reason)
+rapids-logger "Nuking existing global pip config to remove nightly index"
+rm /etc/xdg/pip/pip.conf
+
+rapids-logger "Setting pip global retries to 10"
+pip config --global set global.retries 10
